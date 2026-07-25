10b915d8ce39f4887f643678cca3c98d1e04d104
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-07-25 22:17:23 +0530

    Added

--- searching/linear.py
+++ searching/linear.py
@@ -1,18 +1,18 @@
-arr=list(map(int,input().split()))#[10, 25, 7, 40, 15]
-target=int(input())#40
-for i in range(len(arr)):#5#2#3#4
-    if arr[i]==target:#15==60
-        print("Element found at index",i)#Element found at index 3
+arr=list(map(int,input().split()))
+target=int(input())
+for i in range(len(arr)):
+    if arr[i]==target:
+        print("Element found at index:",i)
         break
-else:
-    print("Element not found")#0(1)
+    else:
+        print("Element not found in the array")
 
 #using functions
 def linear_search(arr,target):
     for i in range(len(arr)):
         if arr[i]==target:
             return i
-    return -1
+    return -1   
 arr=list(map(int,input().split()))
 target=int(input())
 result=linear_search(arr,target)
